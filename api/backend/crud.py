36ce8af86c1341ba491da98a54ded1a48fd4df8b
DolphinChips <td2505052@gmail.com>
2022-12-03 13:35:58 +0500

    CRUD для всего

--- api/backend/crud.py
+++ api/backend/crud.py
@@ -1,18 +1,19 @@
 from sqlalchemy import select
 from sqlalchemy.ext.asyncio import AsyncSession
-import models
+from models import User, Field, Bush, Well, Event, Operation, ExampleOperation
 import schemas
 from security import get_hash_password
+from typing import Sequence
 
 
-async def get_user_by_username(db: AsyncSession, username: str) -> models.User | None:
-    res = (await db.execute(select(models.User).where(models.User.username == username))).scalars().one_or_none()
+async def get_user_by_username(db: AsyncSession, username: str) -> User | None:
+    res = (await db.execute(select(User).where(User.username == username))).scalars().one_or_none()
     return res
 
 
-async def create_user(db: AsyncSession, user: schemas.User) -> models.User:
+async def create_user(db: AsyncSession, user: schemas.User) -> User:
     hashed_password = get_hash_password(user.password)
-    db_user = models.User(
+    db_user = User(
         username=user.username,
         password=hashed_password,
         first_name=user.first_name,
@@ -24,3 +25,27 @@ async def create_user(db: AsyncSession, user: schemas.User) -> models.User:
     await db.commit()
     await db.refresh(db_user)
     return db_user
+
+async def get_fields(db: AsyncSession) -> Sequence[Field]:
+    return (await db.execute(select(Field))).scalars().all()
+
+async def get_field_by_name(db: AsyncSession, name: str) -> Field | None:
+    return (await db.execute(select(Field).where(Field.name == name))).scalars().one_or_none()
+
+async def get_bush_by_id(db: AsyncSession, id: int) -> Bush | None:
+    return (await db.execute(select(Bush).where(Bush.id == id))).scalars().one_or_none()
+
+async def get_well_by_id(db: AsyncSession, id: int) -> Well | None:
+    return (await db.execute(select(Well).where(Well.id == id))).scalars().one_or_none()
+
+async def get_event_by_id(db: AsyncSession, id: int) -> Event | None:
+    return (await db.execute(select(Event).where(Event.id == id))).scalars().one_or_none()
+
+async def get_operation_by_id(db: AsyncSession, id: int) -> Operation | None:
+    return (await db.execute(select(Operation).where(Operation.id == id))).scalars().one_or_none()
+
+async def get_example_operation_by_id(db: AsyncSession, id: int) -> ExampleOperation | None:
+    return (await db.execute(select(ExampleOperation).where(ExampleOperation.id == id))).scalars().one_or_none()
+
+async def get_example_operations_by_name(db: AsyncSession, name: str) -> Sequence[ExampleOperation]:
+    return (await db.execute(select(ExampleOperation).where(ExampleOperation.name == name))).scalars().all()
