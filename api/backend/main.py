3c54d7e27fd74ca46469e06229a953cef6af8835
DolphinChips <td2505052@gmail.com>
2022-12-04 01:51:47 +0500

    Пицца

--- api/backend/main.py
+++ api/backend/main.py
@@ -95,12 +95,26 @@ async def create_event(event: schemas.EventCreate, session: AsyncSession = Depen
     return await crud.create_event(session, event)
 
 
+@app.post("/get_event_by_id", response_model=ErrorModel[schemas.Event], dependencies=[Depends(JWTBearer())])
+async def get_event_by_id(event_id: int, session: AsyncSession = Depends(get_session)):
+    res = await crud.get_event_by_id(session, event_id)
+    if res is None:
+        return {
+            "ok": False,
+            "obj": "Не найдено мероприятие с указанным ID"
+        }
+    return {
+        "ok": True,
+        "obj": res
+    }
+
+
 @app.post("/create_operation", response_model=schemas.Operation, dependencies=[Depends(JWTBearer())])
 async def create_operation(operation: schemas.OperationCreate, session: AsyncSession = Depends(get_session)):
     return await crud.create_operation(session, operation)
 
 
-@app.post("create_example_operation", response_model=schemas.ExampleOperation, dependencies=[Depends(JWTBearer())])
+@app.post("/create_example_operation", response_model=schemas.ExampleOperation, dependencies=[Depends(JWTBearer())])
 async def create_example_operation(operation: schemas.ExampleOperationCreate,
                                    session: AsyncSession = Depends(get_session)):
     return await crud.create_example_operation(session, operation)
@@ -125,12 +139,19 @@ async def update_operation_order(event_id: int, new_order: list[int], session=De
     }
 
 
-@app.post("/add_user", response_model=schemas.User, dependencies=[Depends(JWTBearer()), Depends(admin_required)])
+@app.post("/add_user", response_model=ErrorModel[schemas.User], dependencies=[Depends(JWTBearer()), Depends(admin_required)])
 async def register(user: schemas.User, db=Depends(get_session)):
     db_user = await crud.get_user_by_username(db, user.username)
     if db_user:
-        raise HTTPException(status_code=400, detail="Username already registered")
-    return await crud.create_user(db=db, user=user)
+        return {
+            "ok": False,
+            "obj": "Пользователь уже зарегистрирован"
+        }
+    res = await crud.create_user(db=db, user=user)
+    return {
+        "ok": True,
+        "obj": res
+    }
 
 
 @app.post("/login", response_model=ErrorModel[str])
@@ -140,7 +161,7 @@ async def login(user: schemas.UserLoginSchema, session=Depends(get_session)):
         if verify_password(user.password, res.password):
             return {
                 "ok": True,
-                "res": sign_jwt(res.username)['access_token']
+                "obj": sign_jwt(res.username)['access_token']
             }
     return {
         "ok": False,
